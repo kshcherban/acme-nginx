eb912096b153951842cad58b1153e76fe927e320
kshcherban <k.scherban@gmail.com>
2016-12-16 08:23:46 +0100

    prepared for pypi

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(
     author=u'Konstantin Shcherban',
     packages = find_packages(),
     url='https://github.com/kshcherban/acme-nginx',
+    download_url = 'https://github.com/kshcherban/acme-nginx/tarball/v0.0.3',
     license='GPL v3',
     description='A simple client/tool for Let\'s Encrypt or any ACME server that issues SSL certificates.',
     long_description=open("README.md").read(),
-    keywords="tls ssl certificate acme letsencrypt nginx",
+    keywords=["tls", "ssl", "certificate", "acme", "letsencrypt", "nginx"],
     install_requires=open("requirements.txt").read().split("\n"),
     entry_points={
         'console_scripts': [
