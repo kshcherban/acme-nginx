8bd2f08dfc9b62c923515a8961447e399c10664a
kshcherban <k.scherban@gmail.com>
2016-12-16 08:39:47 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,24 @@
 
 from setuptools import setup, find_packages
 
+p_version = '0.0.4'
+
 setup(
-    name='acme-nginx',
-    version='0.0.3',
-    author=u'Konstantin Shcherban',
+    name = 'acme-nginx',
+    version = p_version,
+    author = 'Konstantin Shcherban',
+    author_email = 'k.scherban@gmail.com',
     packages = find_packages(),
-    url='https://github.com/kshcherban/acme-nginx',
-    download_url = 'https://github.com/kshcherban/acme-nginx/tarball/v0.0.3',
-    license='GPL v3',
-    description='A simple client/tool for Let\'s Encrypt or any ACME server that issues SSL certificates.',
-    keywords=["tls", "ssl", "certificate", "acme", "letsencrypt", "nginx"],
-    install_requires=open("requirements.txt").read().split("\n"),
-    entry_points={
+    url = 'https://github.com/kshcherban/acme-nginx',
+    download_url = 'https://github.com/kshcherban/acme-nginx/tarball/v{0}'.format(p_version),
+    license = 'GPL v3',
+    description = 'A simple client/tool for Let\'s Encrypt or any ACME server that issues SSL certificates.',
+    keywords = ["tls", "ssl", "certificate", "acme", "letsencrypt", "nginx"],
+    install_requires = [
+        "pyOpenSSL>=0.13",
+        "pycrypto>=2.6"
+    ],
+    entry_points = {
         'console_scripts': [
             'acme-nginx = acme_nginx.client:main',
         ]
