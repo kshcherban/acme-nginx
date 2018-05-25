afa7e0bda21121e03ffbafbaae75e080174c35d5
kshcherban <konstantin.shcherban@auto1.com>
2018-05-25 17:49:06 +0200

    Added long description for pypi

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
     download_url = 'https://github.com/kshcherban/acme-nginx/tarball/v{0}'.format(p_version),
     license = 'GPL v3',
     description = 'A simple client/tool for Let\'s Encrypt or any ACME server that issues SSL certificates.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     keywords = ["tls", "ssl", "certificate", "acme", "letsencrypt", "nginx"],
     install_requires = [
         "pyOpenSSL>=0.13",
