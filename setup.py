389e228ee28e11a45398f5b44887f0183208f667
kshcherban <k.scherban@gmail.com>
2018-07-24 00:06:28 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-p_version = '0.0.8'
+p_version = '0.1.0'
 
 setup(
     name = 'acme-nginx',
@@ -13,8 +13,8 @@ setup(
     url = 'https://github.com/kshcherban/acme-nginx',
     download_url = 'https://github.com/kshcherban/acme-nginx/tarball/v{0}'.format(p_version),
     license = 'GPL v3',
-    description = 'A simple client/tool for Let\'s Encrypt or any ACME server that issues SSL certificates.',
-    keywords = ["tls", "ssl", "certificate", "acme", "letsencrypt", "nginx"],
+    description = 'A simple client/tool for Let\'s Encrypt or any ACME server that issues SSL certificates',
+    keywords = ["tls", "ssl", "certificate", "acme", "letsencrypt", "nginx", "wildcard certificate", "wildcard"],
     install_requires = [
         "pyOpenSSL>=0.13",
         "pycrypto>=2.6"
@@ -22,7 +22,6 @@ setup(
     entry_points = {
         'console_scripts': [
             'acme-nginx = acme_nginx.client:main',
-            'acmev2-nginx = acme_nginx.clientv2:main',
         ]
     }
 )
